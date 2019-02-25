5720941a06ab002343bfe7d532de5cd36b16274d
Lucas May Petry <lucasmaypetry@gmail.com>
2019-02-25 16:07:49 -0400

    Added loading animation.

--- static/js/search.js
+++ static/js/search.js
@@ -1,3 +1,13 @@
+$(() => {
+    $("#side-bar").hide();
+    $("#search-btn").click(() => {
+        $("#search-btn").prop("disabled", true);
+        console.log("Search!")
+        const text = $("#search-text").val();
+        search(text);
+    });
+});
+
 function addResult(place) {
   $("#result-list").append(''.concat(`
     <li class="list-group-item d-flex justify-content-between lh-condensed">
@@ -11,15 +21,19 @@ function addResult(place) {
 
 function search(text) {
   $("#res-count").html("");
-  $("#result-list").html("");
+  $("#result-list").html(
+    `<div class="d-flex align-items-center">
+       Loading...
+       <div class="spinner-border ml-auto text-primary" role="status" aria-hidden="true"></div>
+     </div>`);
   $("#side-bar").show();
   params = {
     text: text,
     limit: 10
   };
   $.get('search', params, (data) => {
-      console.log(data);
       $("#res-count").html(data.length);
+      $("#result-list").html('');
       data.forEach(addResult);
       $("#search-btn").prop("disabled", false);
   });
